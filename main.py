418c83ea6c974be5de3a39e66f0131511f89b17f
Alex <alexander.panas.work@gmail.com>
2025-03-01 15:40:20 +0300

    Updated authentication method for GitHub

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message
 from config import BOT_TOKEN
+import asyncio
 
 # Инициализация бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(storage=MemoryStorage())
 
 # Обработчик команды /start
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
+@dp.message()
+async def start_command(message: Message):
     await message.answer("Привет! Я твой Telegram-бот.")
 
 # Эхо-обработчик
-@dp.message_handler()
-async def echo_message(message: types.Message):
+@dp.message()
+async def echo_message(message: Message):
     await message.answer(f"Ты сказал: {message.text}")
 
+# Функция для запуска бота
+async def main():
+    await dp.start_polling(bot)
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
